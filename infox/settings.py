e12a7f996064a14a4f7448407ddfdc59628ea158
Rogério <rogeriorizzon@gmail.com>
2024-11-08 16:21:42 -0300

    testeftp4

--- infox/settings.py
+++ infox/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1'] 
 
 INTERNAL_IPS = ['127.0.0.1']
 
